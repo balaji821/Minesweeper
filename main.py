8f1eb5f0b63bf4e02c914e8ce7dcb37d3315a355
balaji821 <75879172+balaji821@users.noreply.github.com>
2021-04-04 13:09:23 +0530

    Update main.py
    Buttons Destroy on click

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import tkinter as tk
 
 window = tk.Tk()
+i = 0
+j = 0
+buttons = []
 
-buttons = [tk.Button(window,text=str(i)+str(j)).grid(row=i,column=j) for i in range(10) for j in range(10)]
+def action():
+    global i
+    print(buttons)
+    i += 1
+    buttons[i][j].destroy()
+
+# buttons = [tk.Button(window,text=str(i)+str(j),command=action).grid(row=i,column=j) for i in range(10) for j in range(10)]
+for i in range(10):
+    for j in range(10):
+        button = tk.Button(window,text=str(i)+str(j))
+        button.grid(row=i,column=j)
+        button.config(command = button.destroy)
+i = 0
+j = 0
+
 window.mainloop()

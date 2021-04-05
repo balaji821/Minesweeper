837259a957138e46e737ffd8547d859712ddfaf5
balaji821 <75879172+balaji821@users.noreply.github.com>
2021-04-05 16:36:13 +0530

    Updated main.py
    Added expanding functionality on opening an empty spot

--- grid.py
+++ grid.py
@@ -1,13 +1,56 @@
 import random
 
-def generateGrid(rows,cols,n):
-    grid = [[0 for i in range(rows)] for j in range(cols)]
-    for i in range(n):
-        i = random.randint(0,rows-1)
-        j = random.randint(0,cols-1)
-        grid[i][j] = "*"
+def printGrid(grid):
     for i in grid:
         for j in i:
-            print(j,end = " ")
+            print(j,end = "\t")
         print()
-generateGrid(5,5,9)
+
+def getCount(grid,i,j):
+    count = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    if i>0 and grid[i-1][j]=='*':
+        count += 1
+    if i<rows-1 and grid[i+1][j]=='*':
+        count += 1
+    if j>0 and grid[i][j-1]=='*':
+        count += 1
+    if j<cols-1 and grid[i][j+1]=='*':
+        count += 1
+    if i>0 and j>0 and grid[i-1][j-1]=='*':
+        count += 1
+    if i>0 and j<cols-1 and grid[i-1][j+1]=='*':
+        count += 1
+    if i<rows-1 and j>0 and grid[i+1][j-1]=='*':
+        count += 1
+    if i<rows-1 and j<cols-1 and grid[i+1][j+1]=='*':
+        count += 1
+    return count
+
+def generateGrid(rows,cols,n):
+    grid = [[' ' for i in range(rows)] for j in range(cols)]
+    for k in range(n):
+        i = random.randint(0,rows-1)
+        j = random.randint(0,cols-1)
+        while grid[i][j]=='*':
+            i = random.randint(0,rows-1)
+            j = random.randint(0,cols-1)
+        grid[i][j] = "*"
+
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == ' ':
+                x = str(getCount(grid,i,j))
+                if x=='0':
+                    grid[i][j] = ' '
+                else:
+                    grid[i][j] = x
+    
+    # printGrid(grid)
+    return grid
+
+
+generateGrid(10,10,50)
+    
--- main.py
+++ main.py
@@ -1,23 +1,76 @@
 import tkinter as tk
 from functools import partial
+from grid import generateGrid,printGrid
+import math
+
+ROWS = 10
+COLUMNS = 10
+DIFFICULTY = 0.1
+BUTTON_HEIGHT = 30
+BUTTON_WIDTH = 40
+
 
 window = tk.Tk()
+window.geometry('300x400')
 gameGrid = []
+mineGrid = generateGrid(ROWS,COLUMNS,math.ceil((ROWS*COLUMNS)*DIFFICULTY))
+openset=[]
+
+def expand(i,j):
+    rows = len(mineGrid)
+    cols = len(mineGrid[0])
+    if (i,j) in openset:
+        return
+    openset.append((i,j))
+    gameGrid[i][j].destroy()
+    print("Minegrid["+str(i)+"]["+str(j)+"] : "+mineGrid[i][j]+".")
+    if mineGrid[i][j] != ' ':
+        print(i,j)
+        l = tk.Label(window,text=mineGrid[i][j],fg="blue")
+        l.place(x=i*BUTTON_HEIGHT,y = j*BUTTON_WIDTH)
+        l.config(height = 3, width = 3)
+        gameGrid[i][j] = l
+        return
+    if i>0:
+        expand(i-1,j)
+    if i<rows-1:
+        expand(i+1,j)
+    if j>0:
+        expand(i,j-1)
+    if j<cols-1:
+        expand(i,j+1)
+    if i>0 and j>0:
+        expand(i-1,j-1)
+    if i>0 and j<cols-1:
+        expand(i-1,j+1)
+    if i<rows-1 and j>0:
+        expand(i+1,j-1)
+    if i<rows-1 and j<cols-1:
+        expand(i+1,j+1)
+    return
 
 def action(button,i,j):
-    l = tk.Label(window,text=str(i)+str(j),fg="red")
-    l.grid(row=i,column=j)
+    if mineGrid[i][j]==' ':
+        expand(i,j)
+        return
+    l = tk.Label(window,text=mineGrid[i][j],fg="blue")
+    # print(mineGrid)
+    l.place(x=i*BUTTON_HEIGHT,y = j*BUTTON_WIDTH)
+    l.config(height = 2, width = 3)
     gameGrid[i][j] = l
     button.destroy()
+    # print(openset)
 
 # buttons = [tk.Button(window,text=str(i)+str(j),command=action).grid(row=i,column=j) for i in range(10) for j in range(10)]
-for i in range(10):
+for i in range(ROWS):
     row = []
-    for j in range(10):
-        button = tk.Button(window,text=str(i)+str(j))
-        button.grid(row=i,column=j)
+    for j in range(COLUMNS):
+        button = tk.Button(window,text=str(i)+str(j),relief = "raised")
+        button.place(x=i*BUTTON_HEIGHT,y = j*BUTTON_WIDTH)
         button.config(command = partial(action,button,i,j))
+        button.config(height = 2, width = 3)
         row.append(button)
     gameGrid.append(row)
+printGrid(mineGrid)
 
 window.mainloop()
